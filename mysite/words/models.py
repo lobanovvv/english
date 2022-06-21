3fb04e9ad73af80ca4abf640359f3efea0b7e0ad
Anton Lobanov <lobanovvv@mail.ru>
2022-06-21 08:35:23 +0300

    move data into db

--- mysite/words/models.py
+++ mysite/words/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-class CountryNationalitiesLanguage(models.Model):
+class Countries(models.Model):
     country = models.CharField(max_length=100)
     nationalities = models.CharField(max_length=100)
     language = models.CharField(max_length=100)
-    oxford_exist = models.BooleanField(default=False)
+    oxford_chapter = models.CharField(max_length=10)
