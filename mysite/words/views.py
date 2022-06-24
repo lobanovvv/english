5f63ac38d596df53f9f11e83c91df68ed33abd18
Anton Lobanov <lobanovvv@mail.ru>
2022-06-25 01:43:32 +0300

    Output country, mark oxford chapter

--- mysite/words/views.py
+++ mysite/words/views.py
@@ -11,10 +11,10 @@ countries_list = []
 def index(request):
     global countries_list
     if not countries_list:
-        countries_list = list(Countries.objects.values())
+        countries_list = list(Countries.objects.filter(oxford_chapter='A2U1'))
 
     countries_left = len(countries_list)
-    countries_count = len(list(Countries.objects.values()))
+    countries_count = len(list(Countries.objects.filter(oxford_chapter='A2U1')))
     country = countries_list.pop(random.randrange(countries_left))
     template = loader.get_template('words/index.html')
     context = {
