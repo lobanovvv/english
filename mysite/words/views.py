7c63a3291be0e22ddde687b9152497c2e50163a9
Anton Lobanov <lobanovvv@mail.ru>
2022-06-24 23:53:18 +0300

    Output one row from db, and counter.

--- mysite/words/views.py
+++ mysite/words/views.py
@@ -3,13 +3,23 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Countries
+import random
+
+countries_list = []
 
 
 def index(request):
-    countries = Countries.objects.all()
-    country =
+    global countries_list
+    if not countries_list:
+        countries_list = list(Countries.objects.values())
+
+    countries_left = len(countries_list)
+    countries_count = len(list(Countries.objects.values()))
+    country = countries_list.pop(random.randrange(countries_left))
     template = loader.get_template('words/index.html')
     context = {
-        'countries': countries,
+        'country': country,
+        'countries_left': countries_left,
+        'countries_count': countries_count,
     }
     return HttpResponse(template.render(context, request))
