d8ad4ad85011f9d6ca1239f6d92b5360cffdca05
Anton Lobanov <lobanovvv@mail.ru>
2022-06-27 22:43:16 +0300

    create buttons

--- mysite/words/views.py
+++ mysite/words/views.py
@@ -6,27 +6,44 @@ from .models import Countries
 import random
 
 countries_list = []
+correct_answer = ''
 
 
 def index(request):
-    answers = []
     global countries_list
+    global correct_answer
+    answers = []
+
+    if request.POST:
+        if request.POST.get('choice', '') == correct_answer:
+            is_correct = 'CORRECT'
+        else:
+            is_correct = 'WRONG'
 
     if not countries_list:
         countries_list = list(Countries.objects.filter(oxford_chapter='A2U1'))
     answers_list = list(Countries.objects.values('nationalities'))
 
+    # COUNTER
     # Countdown counter
     countries_left = len(countries_list)
     # Count of countries in db
     countries_count = len(list(Countries.objects.filter(oxford_chapter='A2U1')))
-    # Get one country_info and del it from list
+
+    # GET COUNTRY
+    # Get one country information and del it from list
     country_info = countries_list.pop(random.randrange(countries_left))
-    # Answers nationalities
+    # Record country name to global variable for comparison later with customer answer
+    correct_answer = country_info.nationalities
+
+    # ANSWERS NATIONALITIES
+    # Add correct answer
     answers.append(str(country_info.nationalities))
+    # Add random answers
     random_numbers = random.sample(range(countries_count), 3)
     for num in random_numbers:
         answers.append(answers_list.pop(num)['nationalities'])
+    # Shuffle all answers
     random.shuffle(answers)
 
     template = loader.get_template('words/index.html')
@@ -35,5 +52,6 @@ def index(request):
         'countries_left': countries_left,
         'countries_count': countries_count,
         'nationalities': answers,
+        'is_correct': is_correct,
     }
     return HttpResponse(template.render(context, request))
