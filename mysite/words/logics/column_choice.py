611e2df65c70b1cf5419ac64ef0c62d282808093
Anton Lobanov <lobanovvv@mail.ru>
2022-07-24 02:16:15 +0300

    print all properties

--- mysite/words/logics/column_choice.py
+++ mysite/words/logics/column_choice.py
@@ -17,11 +17,13 @@ class ColumnChoice:
         self.answer_column_name = answer_column_name
 
         remained_rows = DB_Reader.get_all_filtered_rows(filter=filter)
-        # count_of_all_rows = rows_left = len(remained_rows)
-        # current_row = remained_rows.pop(random.randrange(rows_left))
-        # print('hello')
-        # print(current_row)
-        # correct_answer = current_row.nationalities
+        print(remained_rows)
+        count_of_all_rows = rows_left = len(remained_rows)
+        print(count_of_all_rows)
+        current_row = remained_rows.pop(random.randrange(rows_left))
+        print(current_row.__dict__)
+        correct_answer = getattr(current_row, 'nationalities')
+        print(correct_answer)
 
 
     def get_current_base_item(self):
